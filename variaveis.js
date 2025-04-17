56a92cb991f6c73ddec8a496bbe1b52c40aeb186
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-04-16 22:19:45 -0300

    declaração de variáveis

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+//declaração variável com var
+/*var nome = "Erick"
+
+var nome = "João"*/
+
+//declaração variável com let
+/*let nome = "Erick"
+
+nome = "João"*/
+
+
+//declaração variável com const
+const nome = "Erick"
+
+nome = "João"
+
+console.log(nome)
