69eaeb0c52a68a42524dcf67f5b3af9645615217
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-05-07 22:19:12 -0300

    feat: exercícios de js

--- DOM/contador/script.js
+++ DOM/contador/script.js
@@ -4,6 +4,15 @@ const resultado = document.querySelector("#resultado")
 
 const btnIncrementaValor = document.querySelector("#btnIncrementar")
 
+const btnDiminuiValor = document.querySelector("#btnDiminui")
+
+btnDiminuiValor.addEventListener("click", () => {
+    if(contador >= 1){
+    contador--
+    resultado.textContent = `Contador: ${contador}`
+}
+})
+
 btnIncrementaValor.addEventListener("click", () =>{
     contador++
     resultado.textContent = `Contador: ${contador}`
